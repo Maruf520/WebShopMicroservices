dce59da0688643ed5780a7711f4333d81fa5246f
Maruf520 <md.maruf5201@gmail.com>
2024-07-07 10:40:40 +0600

    Docker Compose and Carten Added

--- src/Service/Catalog/Catalog.API/Program.cs
+++ src/Service/Catalog/Catalog.API/Program.cs
@@ -6,6 +6,22 @@ builder.Services.AddMediatR( config =>
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
 
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+//var connectionString = builder.Configuration["ConnectionString:Database"];
+//if (string.IsNullOrEmpty(connectionString))
+//{
+//    throw new ArgumentNullException("Database", "Database connection string is missing in the configuration.");
+//}
+
+//builder.Services.AddMarten(opts =>
+//{
+//    opts.Connection(connectionString);
+//}).UseLightweightSessions();
+
+
 var app = builder.Build();
 app.MapCarter();
 
