308583eb16ecc9528dcf8623b7f22d916ec0d355
Maruf520 <md.maruf5201@gmail.com>
2024-07-15 00:06:33 +0600

    Validation Behavior pipeline added

--- src/Service/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Service/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,14 +1,32 @@
 ﻿
+
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler(IDocumentSession session) 
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+                RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+                RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+                RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+                RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+    }
+    internal class CreateProductCommandHandler(IDocumentSession session, ILogger<CreateProductCommand> logger) 
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
+            //var result = await validator.ValidateAsync(command, cancellationToken);
+            //var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+            //if (errors.Any())
+            //{
+            //    throw new ValidationException(errors.FirstOrDefault());
+            //}
+            logger.LogInformation("CreateProductCmmand.Handle called with {@command}", command);
             var product = new Product
             {
                 Name = command.Name,
--- src/Service/Catalog/Catalog.API/Program.cs
+++ src/Service/Catalog/Catalog.API/Program.cs
@@ -1,27 +1,25 @@
+using BuildingBlocks.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCarter();
+var assembly = typeof(Program).Assembly;
+
 builder.Services.AddMediatR( config =>
 {
-    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
+
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddCarter();
 
 builder.Services.AddMarten(opts =>
 {
-    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+   opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
-//var connectionString = builder.Configuration["ConnectionString:Database"];
-//if (string.IsNullOrEmpty(connectionString))
-//{
-//    throw new ArgumentNullException("Database", "Database connection string is missing in the configuration.");
-//}
 
-//builder.Services.AddMarten(opts =>
-//{
-//    opts.Connection(connectionString);
-//}).UseLightweightSessions();
-
-
+  
 var app = builder.Build();
 app.MapCarter();
 
