819c7544ca709a02e8e76f74abb2b1823879ab51
Maruf520 <md.maruf5201@gmail.com>
2024-07-15 01:02:57 +0600

    Global Exception Handler added

---
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/BadRequestException.cs
@@ -0,0 +1,15 @@
+﻿namespace BuildingBlocks.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string messasge) : base(messasge)
+        {
+
+        }
+        public BadRequestException(string messasge, string details) : base(messasge)
+        {
+            Details = details;
+        }
+        public string? Details { get; }
+    }
+}
