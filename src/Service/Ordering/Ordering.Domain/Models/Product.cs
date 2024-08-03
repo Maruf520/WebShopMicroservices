b20e66d894a97edcc6267aed16ec72bff0269076
maruf-bs23 <md.maruf@brainstation-23.com>
2024-08-03 23:07:54 +0600

    Domain Models Created

---
+++ src/Service/Ordering/Ordering.Domain/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Ordering.Domain.Models
+{
+    public class Product : Entity<ProductId>
+    {
+        public string Name { get; private set; } = default!;
+        public string Email { get; private set; } = default!;
+    }
+}
---
+++ src/Service/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -0,0 +1,31 @@
+﻿namespace Ordering.Domain.ValueObjects
+{
+    public record Payment
+    {
+        public string? CardName { get; } = default!;
+        public string CardNumber { get; } = default!;
+        public string Expiration { get; } = default!;
+        public string CVV { get; } = default!;
+        public string PaymentMethod { get; } = default!;
+
+        protected Payment() { }
+        private Payment(string cardName, string cardNumber, string expiratoin, string cVV, string paymentMethod)
+        {
+            CardName = cardName; 
+            CardNumber = cardNumber; 
+            Expiration = expiratoin; 
+            CVV = cVV; 
+            PaymentMethod = paymentMethod;
+        }
+
+        public static Payment Of(string cardName, string cardNumber, string expiratoin, string cVV, string paymentMethod)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(cardName);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cardNumber);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cVV);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(cVV.Length,3);
+
+            return new Payment( cardName,  cardNumber,  expiratoin,  cVV,  paymentMethod);
+        }
+    }
+}
