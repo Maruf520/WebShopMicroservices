60adb057292f0c3026fa3adfe5662d0db5329093
Maruf520 <md.maruf5201@gmail.com>
2024-12-31 19:24:37 +0600

    Order Domain Event Created

--- src/Service/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Service/Ordering/Ordering.Domain/Models/Order.cs
@@ -1,4 +1,6 @@
 ﻿
+using Ordering.Domain.ValueObjects;
+
 namespace Ordering.Domain.Models
 {
     public class Order : Aggregate<OrderId>
@@ -8,7 +10,7 @@ namespace Ordering.Domain.Models
         public CustomerId CustomerId { get; private set; } = default!;
         public OrderName OrderName { get; private set; } = default!;
         public Address ShippingAddress { get; private set; } = default!;
-        public Address BillingAddress { get; private set; } = default!;  
+        public Address BillingAddress { get; private set; } = default!;
         public Payment Payment { get; private set; } = default!;
         public OrderStatus Status { get; private set; } = OrderStatus.Pending;
         public decimal TotalPrice
@@ -17,5 +19,53 @@ namespace Ordering.Domain.Models
             private set { }
         }
 
+
+        public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment)
+        {
+            var order = new Order
+            {
+                Id = id,
+                CustomerId = customerId,
+                OrderName = orderName,
+                ShippingAddress = shippingAddress,
+                BillingAddress = billingAddress,
+                Payment = payment,
+                Status = OrderStatus.Pending
+            };
+
+            order.AddDomainEvent(new OrderCreatedEvent(order));
+
+            return order;
+        }
+
+        public void Update(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment, OrderStatus status)
+        {
+            OrderName = orderName;
+            shippingAddress = shippingAddress;
+            billingAddress = billingAddress;
+            Payment = payment;
+            Status = status;
+
+            AddDomainEvent(new OrderUpdatedEvent(this));
+        }
+
+        public void Add(ProductId productId, int quantity, decimal price)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+            var orderItems = new OrderItem(Id, productId, quantity, price);
+
+            _orderItems.Add(orderItems);
+        }
+
+        public void Remove(ProductId productId)
+        {
+            var orderItem = _orderItems.FirstOrDefault(x => x.ProductId == productId);
+            if (orderItem != null) 
+            {
+                _orderItems.Remove(orderItem);
+            }
+        }
     }
 }
