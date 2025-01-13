bcd2ed9dca41f7521e38b5a9efe71ecc006b8a19
Maruf520 <md.maruf5201@gmail.com>
2025-01-13 19:06:12 +0600

    Order Command and Order Query Added

--- src/Service/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Service/Ordering/Ordering.Application/GlobalUsing.cs
@@ -5,3 +5,8 @@ global using Ordering.Application.Data;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
 global using Ordering.Application.Exceptions;
+global using MediatR;
+global using Microsoft.Extensions.Logging;
+global using Ordering.Domain.Events;
+global using Microsoft.EntityFrameworkCore;
+global using Ordering.Application.Extentions;
---
+++ src/Service/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrderByNameHandler.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Extentions;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByName
+{
+    internal class GetOrderByNameHandler(IApplicationDbContext dbContext) : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResult>
+    {
+        public async Task<GetOrdersByNameResult> Handle(GetOrdersByNameQuery query, CancellationToken cancellationToken)
+        {
+            var orders = await dbContext.Orders.Include(o => o.OrderItems)
+                .AsNoTracking()
+                .Where(x => x.OrderName.Value.Contains(query.name))
+                .OrderBy(x => x.OrderName)
+                .ToListAsync(cancellationToken);
+
+            return new GetOrdersByNameResult(orders.ToOrderDtoList());
+        }
+    }
+}
